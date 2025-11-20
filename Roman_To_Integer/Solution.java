e40612fdc403ae38efea36467f76b5962c873b9b
Bjazz <braxtonjazz05@gmail.com>
2025-11-19 19:06:02 -0500

    Final Version of Roman to Integer

--- Roman_To_Integer/Solution.java
+++ Roman_To_Integer/Solution.java
@@ -18,7 +18,25 @@ public class Solution{
                 finalNum += n1 - n2;
             }
 
-            // If not a unique combination add the pair to the final sum
+            // If n1 and n2 don't represent a proper numeral pair
+            // Add n1 to the final number and start at n2's position in the next iteration
+            else if(n2 / n1 != getFirstDigit(n2)){
+                finalNum += n1;
+                i++;
+
+                // If firstNum was not set then there were odd number of pairs
+                // However, with the current pair not being valid there are now even pairs
+                if (firstNum == 0){
+                    firstNum = convertRomanToNumeric(numerals[0]);
+                }
+
+                // The firstNum was set, therefore there were even pairs, but now there are odd
+                else{
+                    firstNum = 0;
+                }
+            }
+
+            // If not a unique combination add the pair to the final number
             else{
                 finalNum += n1 + n2;
             }
@@ -63,5 +81,14 @@ public class Solution{
         else{
             return 1000;
         }
+    }
+
+    // Gets the first digit of a number
+    private static int getFirstDigit(int n){
+        while (n > 9){
+            n /= 10;
+        }
+
+        return n;
     }
 }
--- Roman_To_Integer/TestSolution.java
+++ Roman_To_Integer/TestSolution.java
@@ -2,7 +2,9 @@ package Roman_To_Integer;
 
 public class TestSolution {
     public static void main(String[] args){
-        String testRomanNumeral = "MCMXCIV";
+        // String testRomanNumeral = "MCMXCIV";
+        String testRomanNumeral = "MCDLXXVI";
+        
         
         System.out.println(Solution.romanToInt(testRomanNumeral));
     }
